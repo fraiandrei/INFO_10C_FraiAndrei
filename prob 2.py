0a19635d6d08bc061b05781a02e8a340642a9eb6
fraiandrei <acierdnay@gmail.com>
2026-09-15 10:09:03 +0300

    Add files via upload

---
+++ prob 2.py
@@ -0,0 +1,8 @@
+a=5
+b=3
+print('suma',a,'+',b, '=', a+b,end= '\n')
+print('produsu',a,'*',b, '=', a*b)
+print('diferenta',a,'-',b, '=', a-b)
+print('catul',a,'/',b, '=', a/b)
+print('restu',a,'//',b, '=', a//b)
+print('diferenta',a,'**',b, '=', a**b)
